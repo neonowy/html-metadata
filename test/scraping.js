b5d5062ade7c0c3d46512eea7df603962bff8f90
Krzysztof Zbudniewek <krzysztof.zbudniewek@gmail.com>
2015-12-17 19:04:24 +0100

    Add scraping of Highwire Press tags

--- test/scraping.js
+++ test/scraping.js
@@ -51,7 +51,7 @@ describe('scraping', function() {
 		return meta(url)
 		.catch(function(e){throw e;})
 		.then(function(res) {
-			var expectedImage = '{"url":"http://s1.lemde.fr/medias/web/1.2.679/img/placeholder/opengraph.jpg"}';
+			var expectedImage = '{"url":"http://s1.lemde.fr/medias/web/1.2.682/img/placeholder/opengraph.jpg"}';
 			assert.deepEqual(JSON.stringify(res.openGraph.image), expectedImage);
 		});
 	});
@@ -75,6 +75,52 @@ describe('scraping', function() {
 			.then(function(results){
 				assert.deepEqual(JSON.stringify(results), expected);
 			});
+		});
+	});
+
+	it('should get basic Highwire Press metadata', function() {
+		url = 'http://mic.microbiologyresearch.org/content/journal/micro/10.1099/mic.0.26954-0';
+		return preq.get(url).then(function(callRes) {
+			var expectedAuthors = [
+				'Jacqueline M. Reimers',
+				'Karen H. Schmidt',
+				'Angelika Longacre',
+				'Dennis K. Reschke',
+				'Barbara E. Wright'
+			];
+
+			var chtml = cheerio.load(callRes.body);
+
+			return meta.parseHighwirePress(chtml)
+			.then(function(results) {
+				['journal_title', 'issn', 'doi', 'publication_date', 'title', 'author', 'author_institution',
+				 'volume', 'issue', 'firstpage', 'lastpage', 'publisher', 'abstract', 'reference'].forEach(function(key) {
+					if(!results[key]) {
+						throw new Error('Expected to find the ' + key + ' key in the response!');
+					}
+				});
+			})
+		});
+	})
+
+	it('should get Highwire Press author tags correctly', function() {
+		url = 'http://mic.microbiologyresearch.org/content/journal/micro/10.1099/mic.0.26954-0';
+		return preq.get(url).then(function(callRes) {
+			var expectedAuthors = [
+				'Jacqueline M. Reimers',
+				'Karen H. Schmidt',
+				'Angelika Longacre',
+				'Dennis K. Reschke',
+				'Barbara E. Wright'
+			];
+
+			var chtml = cheerio.load(callRes.body);
+
+			return meta.parseHighwirePress(chtml)
+			.then(function(results) {
+				var authors = results.authors;
+				assert.deepEqual(JSON.stringify(authors), authors);
+			})
 		});
 	});
 
